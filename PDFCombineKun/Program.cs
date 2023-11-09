239b1d9f5dd024c05711d0d235d141a1ea65e74e
ritsuxis <work@ritsuxis.net>
2023-11-09 12:06:07 +0900

    initial commit

---
+++ PDFCombineKun/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDFCombineKun
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string defaultOutputPdfFile = "output.pdf";
+            var outputPdfFile = defaultOutputPdfFile;
+            var inputPdfFiles = new List<string>();
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                // -o で出力ファイルの名前を指定
+                if (args[i].Equals("-o", StringComparison.OrdinalIgnoreCase) && i + 1 < args.Length)
+                {
+                    outputPdfFile = args[i + 1];
+                    i++; // 次の引数も処理済みとしてスキップ
+                }
+                else
+                {
+                    inputPdfFiles.Add(args[i]);
+                }
+            }
+
+            if (inputPdfFiles.Count < 1)
+            {
+                Console.WriteLine("少なくとも1つの入力ファイルを指定してください。");
+                return;
+            }
+
+            MergePdfFiles(inputPdfFiles, outputPdfFile);
+
+            Console.WriteLine($"PDFファイルの結合が完了しました。出力ファイル: {outputPdfFile}");
+        }
+
+        private static void MergePdfFiles(IEnumerable<string> inputPdfFiles, string outputPdfFile)
+        {
+            var document = new Document();
+            var copy = new PdfCopy(document, new FileStream(outputPdfFile, FileMode.Create));
+
+            document.Open();
+
+            foreach (var reader in inputPdfFiles.Select(inputFile => new PdfReader(inputFile)))
+            {
+                for (var page = 1; page <= reader.NumberOfPages; page++)
+                {
+                    var importedPage = copy.GetImportedPage(reader, page);
+                    copy.AddPage(importedPage);
+                }
+
+                reader.Close();
+            }
+
+            document.Close();
+        }
+    }
+}
---
+++ PDFCombineKun/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PDFCombineKun")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PDFCombineKun")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("BC1ED147-4370-495A-ADE1-4955388FC071")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
